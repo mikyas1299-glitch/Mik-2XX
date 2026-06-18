62878404bd2c575a6344611a546bc13420277c7f
mikyas1299-glitch <mikyas1299@gmail.com>
2026-06-18 21:16:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,18 +37,3 @@ section.classList.add("show");
 });
 
 });
-
-const links = document.querySelectorAll(".menu a");
-const sections = document.querySelectorAll(".page-section");
-
-links.forEach(link => {
-    link.addEventListener("click", () => {
-
-        sections.forEach(section => {
-            section.style.display = "none";
-        });
-
-        const target = document.querySelector(link.getAttribute("href"));
-        target.style.display = "block";
-    });
-});
