9a30f2e691ec4339385efe75e9e42a5cad4642ad
mikyas1299-glitch <mikyas1299@gmail.com>
2026-06-18 20:19:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,26 @@ section.classList.add("show");
 });
 
 });
+
+const links = document.querySelectorAll(".menu a");
+const sections = document.querySelectorAll(".page-section");
+
+links.forEach(link => {
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+
+        const target = this.getAttribute("href").substring(1);
+
+        sections.forEach(section => {
+            section.style.display = "none";
+        });
+
+        const activeSection = document.getElementById(target);
+
+        if(target === "home"){
+            activeSection.style.display = "flex";
+        }else{
+            activeSection.style.display = "block";
+        }
+    });
+});
