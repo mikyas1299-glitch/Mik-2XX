70bbd1c5b35c099cb29e530f9b9bee53c8493340
mikyas1299-glitch <mikyas1299@gmail.com>
2026-06-18 15:14:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ setTimeout(typing,100);
 }
 
 typing();
+
+const sections =
+document.querySelectorAll("section");
+
+window.addEventListener("scroll",()=>{
+
+sections.forEach(section=>{
+
+const top=
+section.getBoundingClientRect().top;
+
+if(top < window.innerHeight-100){
+
+section.classList.add("show");
+
+}
+
+});
+
+});
